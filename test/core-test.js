cab408057dfd8900dca9ede51d764fdb124a5771
jcode <j.code@qq.com>
2014-12-07 21:10:58 +0800

    add: 添加纯数字正则单元测试.

--- test/core-test.js
+++ test/core-test.js
@@ -12,4 +12,15 @@ describe('REGEXPS test', function() {
 		assert(REGEXPS.email.test('27301490@qq.com'), '27301490@qq.com');
 		assert(REGEXPS.email.test('abc@de_dt.cn') == false, '域名不能带下划线');
 	});
+
+	it('number regexp', function() {
+		assert(REGEXPS.number.test('12345'), '12345');
+		assert(REGEXPS.number.test('12.345'), '12.345');
+		assert(REGEXPS.number.test('-234'), '-234');
+		assert(REGEXPS.number.test('-0.24'), '-0.25');
+		assert(REGEXPS.number.test('.34'), '.34');
+		assert(REGEXPS.number.test('-.34'), '-.34');
+		assert(REGEXPS.number.test('3-4') == false, '3-4!');
+		assert(REGEXPS.number.test('3.4.4') == false, '3.4.4!');
+	})
 })
