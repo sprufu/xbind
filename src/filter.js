9741d9e2c8c8301d41f5195642747138531ac08f
jcode <j.code@qq.com>
2014-12-09 12:39:24 +0800

    add: 添加解析日期函数parseDate, parseDateString, parseDateNumber.

--- src/filter.js
+++ src/filter.js
@@ -8,7 +8,8 @@ var FILTERS = {
 	 * name: function(obj, arg...),
 	 */
 	date: function(obj, format) {
-		// TODO
+        var date = parseDate(obj);
+        return formatDate(date, format);
 	},
 
 	/**
@@ -28,5 +29,72 @@ var FILTERS = {
 		return it.toFixed(num);
 	}
 };
+
+/**
+ * 把时间字符串解析成Date对象
+ * 时间对象直接返回
+ * 数字当成时间戳解析, 但10位和13位是不同的, 10位表示秒, 13位表示毫秒
+ * 其它返回null
+ */
+function parseDate(obj) {
+    if (!obj) {
+        return null;
+    }
+
+    switch(exports.type(obj)) {
+        case 'string': return parseDateString(obj);
+        case 'date': return obj;
+        case 'number': return parseDateNumber(obj);
+        default: return null;
+    }
+}
+
+/**
+ * 解析日期字符串
+ * 字符串就应该符合如下格式:
+ *      yyyy/mm/dd[ hh:MM:ss] yyyy-mm-dd[ hh:MM:ss]
+ *      mm/dd/yyyy[ hh:MM:ss] mm-dd-yyyy[ hh:MM:ss]
+ *      2014-12-09T03:24:08.539Z
+ */
+function parseDateString(str) {
+    var time = Date.parse(str);
+    if (!isNaN(time)) {
+        return parseDateNumber(time);
+    }
+
+    // 不可解析的字符串处理
+
+    /* ie678( */
+    // ie678中, 把"-"转化为"/"
+    time = Date.parse(str.replace(/-/g, '/'));
+    if (!isNaN(time)) {
+        return parseDateNumber(time);
+    }
+
+    // JSON解析
+    var jsonDateRegexp = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}\:\d{2}:\d{2})\.(\d{3})Z$/,
+    match = str.match(jsonDateRegexp);
+    if (match) {
+        return parseDateNumber(Date.parse(match[1] + '/' + match[2] + '/' + match[3] + ' ' + match[4]) + (+match[5]));
+    }
+    /* ie678) */
+
+    return null;
+}
+
+/**
+ * 把时间戳转化为日期对象
+ * 小于1e11的x1000再转换
+ */
+function parseDateNumber(num) {
+    if (num < 1e11) {
+        num *= 1000;
+    }
+    return new Date(num);
+}
+
+function formatDate(date, format) {
+    // TODO
+}
 
 // vim:et:sw=4:ft=javascript:ff=dos:fenc=utf-8:ts=4:noswapfile
